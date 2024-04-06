cdcb29133812f429c7c0a7d62da7ecdc87c915c7
ignacioo <ignacioo@DESKTOP-TV98TAS>
2024-04-06 01:11:42 -0300

    se agrega paquete clase para el ejercicio 1

---
+++ TP1_ignacioSuruguay/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,19 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+    public static void main(String[] args) {
+        // Declarar e inicializar las variables
+        int num1 = 8;
+        int num2 = 5;
+        int num3 = 1;
+        int num4 = 20;
+        int num5 = 9;
+        
+        // Calcular el promedio
+        double promedio = (num1 + num2 + num3 + num4 + num5) / 5.0;
+        
+        // Mostrar el resultado por consola
+        System.out.println("El promedio es: " + promedio);
+    }
+}
+ 
